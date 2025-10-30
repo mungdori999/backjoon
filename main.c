9e2ab87a54850b169c26df6531f2c0cc56f720da
mungdori <mungdori999@gmail.com>
2025-10-30 20:51:10 +0900

    2480

--- main.c
+++ main.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b = 0;
-    int term = 0;
-    scanf("%d %d", &a, &b);
-    scanf("%d", &term);
+    int a, b, c = 0;
+    scanf("%d %d %d", &a, &b, &c);
 
-    b = b + term;
-    int limit = b / 60;
+    int scoreA = a - b;
+    int scoreB = c - b;
+    int scoreC = a - c;
 
-    if (limit > 0) {
-        a += limit;
-        b -= (limit * 60);
-        if (a >= 24) {
-            a -= 24;
+    int flag = scoreA * scoreB * scoreC;
+
+    int final = 0;
+
+    if (flag == 0) {
+        if (scoreA == 0 && scoreB == 0 && scoreC == 0) {
+            final = a;
+            printf("%d", 10000 + (final * 1000));
+        } else {
+
+            if (a == b) {
+                final = a;
+            } else if (b == c) {
+                final = b;
+            } else {
+                final = c;
+            }
+            printf("%d", 1000 + (final * 100));
         }
+
+
+    } else {
+        int final = a;
+        if (b > final) final = b;
+        if (c > final) final = c;
+
+        printf("%d",  (final * 100));
     }
-    printf("%d %d", a, b);
+
+
     return 0;
 }
