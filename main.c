a1f4544878bd1d8d98637ea2496990dbaeceab43
mungdori <mungdori999@gmail.com>
2025-10-31 16:08:40 +0900

    10818

--- main.c
+++ main.c
@@ -3,23 +3,26 @@
 
 int main(void) {
 
-    int n, x = 0;
-    scanf("%d %d", &n, &x);
+    int n = 0;
+    scanf("%d", &n);
 
-    int arr[n];
+    long arr[n];
     memset(arr, 0, sizeof(arr));
 
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &arr[i]);
 
-    }
-    int target[n];
-    memset(target, 0, sizeof(target));
 
     for (int i = 0; i < n; ++i) {
-        if (arr[i] < x) printf("%d ", arr[i]);
+        scanf("%ld", &arr[i]);
     }
 
+    long max = arr[0];
+    long min = arr[0];
 
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] >= max) max = arr[i];
+        if (arr[i] <= min) min = arr[i];
+    }
+
+    printf("%ld %ld", min, max);
     return 0;
 }
