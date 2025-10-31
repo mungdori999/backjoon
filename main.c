cf657cd07ebea7e6e34c0aa4e49423951bc8da5f
mungdori <mungdori999@gmail.com>
2025-10-31 14:24:44 +0900

    2739

--- main.c
+++ main.c
@@ -1,42 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b, c = 0;
-    scanf("%d %d %d", &a, &b, &c);
 
-    int scoreA = a - b;
-    int scoreB = c - b;
-    int scoreC = a - c;
+    int a = 0;
 
-    int flag = scoreA * scoreB * scoreC;
+    scanf("%d", &a);
 
-    int final = 0;
-
-    if (flag == 0) {
-        if (scoreA == 0 && scoreB == 0 && scoreC == 0) {
-            final = a;
-            printf("%d", 10000 + (final * 1000));
-        } else {
-
-            if (a == b) {
-                final = a;
-            } else if (b == c) {
-                final = b;
-            } else {
-                final = c;
-            }
-            printf("%d", 1000 + (final * 100));
-        }
-
-
-    } else {
-        int final = a;
-        if (b > final) final = b;
-        if (c > final) final = c;
-
-        printf("%d",  (final * 100));
+    for (int i = 1; i < 10; i++) {
+        printf("%d * %d = %d\n", a, i, a * i);
     }
-
 
     return 0;
 }
