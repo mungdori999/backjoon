09f001462f640a26b013bfb3ae08c1823d068e49
mungdori <mungdori999@gmail.com>
2025-10-31 14:43:38 +0900

    25314

--- main.c
+++ main.c
@@ -2,24 +2,15 @@
 
 int main(void) {
 
-    int x, n = 0;
-
-    scanf("%d", &x);
+    int n = 0;
 
     scanf("%d", &n);
 
-    int sum = 0;
-
-    for (int i = 0; i < n; i++) {
-        int a, b = 0;
-
-        scanf("%d %d", &a, &b);
-
-        sum += (a * b);
-
+    for (int i = 0; i < (n / 4); i++) {
+        printf("long ");
     }
+    printf("int");
 
-    printf("%s", x == sum?  "Yes": "No");
 
 
     return 0;
