4930292250388c7bcc23e3fa5d0731c3a75f062d
mungdori <mungdori999@gmail.com>
2025-10-31 15:25:58 +0900

    10951

--- main.c
+++ main.c
@@ -4,9 +4,8 @@ int main(void) {
 
     int a, b = 0;
 
-    while (1) {
-        scanf("%d %d", &a, &b);
-        if (a == 0 && b == 0) break;
+    while (scanf("%d %d", &a, &b) != EOF) {
+
         printf("%d\n", a + b);
     }
 
