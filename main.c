f50c93c0879102aac15e84cddcf03d49d051815a
mungdori <mungdori999@gmail.com>
2025-10-31 15:17:32 +0900

    2439

--- main.c
+++ main.c
@@ -7,7 +7,10 @@ int main(void) {
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (int k = 0; k < i; ++k) {
             printf("*");
         }
         printf("\n");
