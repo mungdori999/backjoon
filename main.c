7846e46634030f763e1f79296848f3cc7027479b
mungdori <mungdori999@gmail.com>
2025-10-31 15:34:04 +0900

    10807

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
-    int a, b = 0;
+    int n = 0;
+    scanf("%d", &n);
 
-    while (scanf("%d %d", &a, &b) != EOF) {
+    int arr[n];
 
-        printf("%d\n", a + b);
+    memset(arr, 0, sizeof(arr));
+
+    for (int i = 0; i < n; ++i) {
+        int a = 0;
+        scanf("%d ", &a);
+        arr[i] = a;
     }
+    int target, count = 0;
+
+    scanf("%d", &target);
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == target) count++;
+    }
+
+    printf("%d",count);
 
 
     return 0;
