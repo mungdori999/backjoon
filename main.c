aedd47aa0d72d899cd1358f68f373e173f492965
mungdori <mungdori999@gmail.com>
2025-11-05 17:59:33 +0900

    10809

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    int num;
-    scanf("%d", &num);
 
     char st[101];
     scanf("%s", st);
 
-    int sum = 0;
-    for (int i = 0; i < num; ++i) {
-        sum += st[i] - '0';
+    int target = 97;
+    int seq = -1;
+
+
+    for (int i = 0; i < 26; ++i) {
+        for (int j = 0; j < strlen(st); ++j) {
+            if (st[j] == target) {
+                seq = j;
+                break;
+            }
+        }
+
+        printf("%d ", seq);
+        target++;
+        seq = -1;
     }
 
-    printf("%d", sum);
     return 0;
 }
