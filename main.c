13c8af7290ea6db8e34af04cfd4a9ca8bfb5a877
mungdori <mungdori999@gmail.com>
2025-11-07 15:52:33 +0900

    3003

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+    int piece[6] ={1,1,2,2,2,8};
 
-    printf("         ,r'\"7 \n");
-    printf("r`-_   ,'  ,/ \n");
-    printf(" \\. \". L_r' \n");
-    printf("   `~\\/ \n");
-    printf("      | \n");
-    printf("      | \n");
+    for (int i = 0; i < 6; ++i) {
+        int find;
+        scanf("%d",&find);
+
+        printf("%d ",piece[i]-find);
+    }
+
     return 0;
 }
