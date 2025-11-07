b8bf0997224b04c10b3aa8cc9b8d969bee00e0db
mungdori <mungdori999@gmail.com>
2025-11-07 17:16:06 +0900

    1157

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void) {
-    char word[101];
-    int pellin = 1;
+    char word[1000001];
+    int counts[26];
+    memset(counts, 0, sizeof(counts));
 
     scanf("%s", word);
 
-    for (int i = 0; i < strlen(word) / 2; ++i) {
-        if (word[i] != word[strlen(word) - 1 - i]) {
-            pellin = 0;
-            break;
-        } else {
-            pellin = 1;
+    int len = strlen(word);
+
+    for (int i = 0; i < len; ++i) {
+        char upperChar = toupper(word[i]);
+        counts[upperChar - 'A']++;
+    }
+    int max = 0;
+    int num = 0;
+
+    for (int i = 0; i < 26; ++i) {
+        if (counts[i] > max) {
+            max = counts[i];
+            num = i;
         }
     }
-    printf("%d", pellin);
+    int count = 0;
+    for (int i = 0; i < 26; ++i) {
+        if (max == counts[i]) count++;
+        if(count>1) break;
+    }
+
+    printf("%c", count > 1 ? '?' : num + 'A');
 
     return 0;
 }
