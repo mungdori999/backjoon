37592a491becdc611a03946ef52fe04573cb70bc
mungdori <mungdori999@gmail.com>
2025-11-08 11:13:01 +0900

    2941

--- main.c
+++ main.c
@@ -1,36 +1,62 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main(void) {
-    char word[1000001];
-    int counts[26];
-    memset(counts, 0, sizeof(counts));
 
+    char alphabet[8][3] = {{'c', '=', '0'},
+                           {'c', '-', '0'},
+                           {'d', 'z', '='},
+                           {'d', '-', '0'},
+                           {'l', 'j', '0'},
+                           {'n', 'j', '0'},
+                           {'s', '=', '0'},
+                           {'z', '=', '0'}};
+
+    char word[110];
+    int count = 0;
     scanf("%s", word);
 
     int len = strlen(word);
 
+
     for (int i = 0; i < len; ++i) {
-        char upperChar = toupper(word[i]);
-        counts[upperChar - 'A']++;
-    }
-    int max = 0;
-    int num = 0;
+        int check = 0;
+        int three = 0;
 
-    for (int i = 0; i < 26; ++i) {
-        if (counts[i] > max) {
-            max = counts[i];
-            num = i;
+        for (int j = 0; j < 8; ++j) {
+
+            for (int k = 0; k < 2; ++k) {
+                if (word[i + k] == alphabet[j][k]) {
+                    check = 1;
+                } else {
+                    check = 0;
+                    break;
+                }
+            }
+
+            if (check == 1) {
+                if (j == 2) {
+                    if (word[i + 2] == alphabet[j][2]) {
+                        three = 1;
+                    } else if (word[i + 2] != alphabet[j][2]) {
+                        three = 0;
+                        check = 0;
+                    }
+                }
+                break;
+            }
+
+
         }
-    }
-    int count = 0;
-    for (int i = 0; i < 26; ++i) {
-        if (max == counts[i]) count++;
-        if(count>1) break;
+        if (check == 1 && three == 0) {
+            i++;
+        } else if (check == 1 && three == 1) {
+            i += 2;
+        }
+        count++;
+
     }
 
-    printf("%c", count > 1 ? '?' : num + 'A');
-
+    printf("%d", count);
     return 0;
 }
