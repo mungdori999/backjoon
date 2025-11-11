e96d338d0d469a2191a5034c858214c65ecb73c8
mungdori <mungdori999@gmail.com>
2025-11-11 16:46:09 +0900

    1316

--- main.c
+++ main.c
@@ -3,60 +3,44 @@
 
 int main(void) {
 
-    char alphabet[8][3] = {{'c', '=', '0'},
-                           {'c', '-', '0'},
-                           {'d', 'z', '='},
-                           {'d', '-', '0'},
-                           {'l', 'j', '0'},
-                           {'n', 'j', '0'},
-                           {'s', '=', '0'},
-                           {'z', '=', '0'}};
+    int n;
+    scanf("%d", &n);
 
-    char word[110];
     int count = 0;
-    scanf("%s", word);
 
-    int len = strlen(word);
+    for (int i = 0; i < n; ++i) {
+        char word[101];
+        scanf("%s", word);
 
+        int len = strlen(word);
 
-    for (int i = 0; i < len; ++i) {
-        int check = 0;
-        int three = 0;
+        int flag = 0;
+        int check = 1;
 
-        for (int j = 0; j < 8; ++j) {
-
-            for (int k = 0; k < 2; ++k) {
-                if (word[i + k] == alphabet[j][k]) {
-                    check = 1;
+        for (int j = 0; j < len; ++j) {
+            flag = 0;
+            for (int k = j + 1; k < len; ++k) {
+                if (flag == 1) {
+                    if (word[j] == word[k]) {
+                        check = 0;
+                        break;
+                    }
                 } else {
-                    check = 0;
-                    break;
+                    if (word[j] != word[k]) {
+                        flag = 1;
+                    }
                 }
             }
 
-            if (check == 1) {
-                if (j == 2) {
-                    if (word[i + 2] == alphabet[j][2]) {
-                        three = 1;
-                    } else if (word[i + 2] != alphabet[j][2]) {
-                        three = 0;
-                        check = 0;
-                    }
-                }
+            if (check == 0) {
                 break;
             }
 
-
         }
-        if (check == 1 && three == 0) {
-            i++;
-        } else if (check == 1 && three == 1) {
-            i += 2;
+        if (check == 1) {
+            count++;
         }
-        count++;
-
     }
-
     printf("%d", count);
     return 0;
 }
