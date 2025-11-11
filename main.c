deb08dcf25ac71fc3a13f6ca93d4937cf9057a9c
mungdori <mungdori999@gmail.com>
2025-11-11 19:23:12 +0900

    2738

--- main.c
+++ main.c
@@ -1,54 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
 
-    float sumScore = 0;
-    float sumPoint = 0;
-    for (int i = 0; i < 20; ++i) {
-        char sub[51];
-        float point;
-        char rating[3];
+    int n, m;
+    scanf("%d %d", &n, &m);
 
-        scanf("%s %f %s", sub, &point, rating);
+    int arr1[n][m];
+    int arr2[n][m];
 
-        if (rating[0] == 'P') continue;
-
-        sumPoint += point;
-
-        if (rating[0] == 'A') {
-            if (rating[1] == '+') {
-                sumScore += (4.5f * point);
-
-            } else {
-                sumScore += (4.0f * point);
-            }
-        } else if (rating[0] == 'B') {
-            if (rating[1] == '+') {
-                sumScore += (3.5f * point);
-
-            } else {
-                sumScore += (3.0f * point);
-            }
-        } else if (rating[0] == 'C') {
-            if (rating[1] == '+') {
-                sumScore += (2.5f * point);
-
-            } else {
-                sumScore += (2.0f * point);
-            }
-        } else if (rating[0] == 'D') {
-            if (rating[1] == '+') {
-                sumScore += (1.5f * point);
-
-            } else {
-                sumScore += (1.0f * point);
-            }
-        } else {
-            sumScore += 0.0f;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            int a;
+            scanf("%d", &a);
+            arr1[i][j] = a;
         }
     }
 
-    printf("%f", sumScore / sumPoint);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            int a;
+            scanf("%d", &a);
+            arr2[i][j] = a;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%d ",arr1[i][j]+arr2[i][j]);
+        }
+        printf("\n");
+    }
+
+
     return 0;
 }
