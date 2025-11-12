e4024439fc74aa58c968ef9caec70a9864ccdab8
mungdori <mungdori999@gmail.com>
2025-11-12 17:01:39 +0900

    10798

--- main.c
+++ main.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
-    int cross[10][10];
+    char arr[5][16];
+    memset(arr, 0, sizeof(arr));
 
-    for (int i = 1; i <= 9; ++i) {
-        for (int j = 1; j <= 9; ++j) {
-            scanf("%d", &cross[i][j]);
-        }
-    }
-    int max = 0;
-    int x, y;
-    for (int i = 1; i <= 9; ++i) {
-        for (int j = 1; j <= 9; ++j) {
-            if (cross[i][j] >= max) {
-                max = cross[i][j];
-                x = i;
-                y = j;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 15; ++j) {
+            char ch = getchar();
+            if (ch == '\n' || ch == EOF) break;
+            arr[i][j] = ch;
+
+            if (j == 14) {
+                while ((ch = getchar()) != '\n' && ch != EOF);
+                break;
             }
         }
     }
 
-    printf("%d\n", max);
-    printf("%d %d", x, y);
-
+    for (int i = 0; i < 15; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            if(arr[j][i]==0) continue;
+            printf("%c", arr[j][i]);
+        }
+    }
 
     return 0;
 }
