3319a46b4a6b080142dacf9a8c1630c501584171
mungdori <mungdori999@gmail.com>
2025-11-13 22:04:50 +0900

    2720

--- main.c
+++ main.c
@@ -3,33 +3,26 @@
 #include <tgmath.h>
 
 int main(void) {
+    int n, pay;
+    int quarter, dime, nickel, penny;
 
-    int decimal, radix;
-
-    scanf("%d %d", &decimal, &radix);
-
-    int quotient = decimal;
-
-    int remainder[200] = {0,};
-    int n = 0;
-
-    if (quotient == 0) {
-        printf("%d", 0);
-        return 0;
-    }
-    while (quotient != 0) {
-        remainder[n] = quotient % radix;
-        quotient = quotient / radix;
-        n++;
-    }
-
-    char output[200];
+    scanf("%d", &n);
 
     for (int i = 0; i < n; ++i) {
-        if (remainder[n - i - 1] >= 10) output[i] = 'A' + (remainder[n - i - 1] - 10);
-        else output[i] = remainder[n - i - 1] + '0';
+        scanf("%d", &pay);
 
-        printf("%c", output[i]);
+        quarter = pay / 25;
+        pay -= (quarter * 25);
+
+        dime = pay / 10;
+        pay -= (dime * 10);
+
+        nickel = pay / 5;
+        pay -= (nickel * 5);
+
+        penny = pay / 1;
+        pay -= (penny);
+        printf("%d %d %d %d\n", quarter, dime, nickel, penny);
     }
 
     return 0;
