f4fc2f11a0ec79919c3a39fc5cab55591b683c0f
mungdori <mungdori999@gmail.com>
2025-11-13 19:49:33 +0900

    2745

--- main.c
+++ main.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <tgmath.h>
 
 int main(void) {
 
-    int n;
-    int coordinate[100][100];
+    char input[100];
+    int radix;
+    int decimal = 0;
 
+    scanf("%s %d", input, &radix);
 
-    memset(coordinate, 0, sizeof(coordinate));
+    int len = strlen(input);
+    int digits = len - 1;
+    for (int i = 0; i < len; ++i) {
+        int num = 0;
+        if ((int) input[i] >= 65) num = input[i] - 'A' + 10;
+        else { num = input[i] - '0'; }
 
-    scanf("%d", &n);
-    int area = 0;
-
-    for (int i = 0; i < n; ++i) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-
-        for (int j = 0; j < 10; ++j) {
-            for (int k = 0; k < 10; ++k) {
-                coordinate[x + j][y + k] = 1;
-            }
-        }
+        decimal += (num * pow(radix,digits));
+        digits--;
     }
 
-    for (int i = 0; i < 100; ++i) {
-        for (int j = 0; j < 100; ++j) {
-            if (coordinate[i][j] == 1) area++;
-        }
-    }
 
-    printf("%d",area);
+    printf("%d", decimal);
     return 0;
 }
