a9a4861acc9289953bc68562192c68ea9be4d960
mungdori <mungdori999@gmail.com>
2025-11-14 16:25:35 +0900

    2903

--- main.c
+++ main.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-#include <string.h>
-#include <tgmath.h>
 
 int main(void) {
-    int n, pay;
-    int quarter, dime, nickel, penny;
+    int n;
 
     scanf("%d", &n);
 
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &pay);
 
-        quarter = pay / 25;
-        pay -= (quarter * 25);
-
-        dime = pay / 10;
-        pay -= (dime * 10);
-
-        nickel = pay / 5;
-        pay -= (nickel * 5);
-
-        penny = pay / 1;
-        pay -= (penny);
-        printf("%d %d %d %d\n", quarter, dime, nickel, penny);
+    int dot = 3;
+    for (int i = 1; i < n; ++i) {
+        dot = dot * 2 - 1;
     }
+
+    printf("%d", dot * dot);
 
     return 0;
 }
