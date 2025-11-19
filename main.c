17b9433a1b599bda3e4de9c0d9a8372e99599425
mungdori <mungdori999@gmail.com>
2025-11-19 18:46:40 +0900

    1978

--- main.c
+++ main.c
@@ -2,39 +2,23 @@
 
 int main(void) {
     int A;
+    int count = 0;
 
-    while (1) {
-        int arr[100000] = {0,};
-        int count = 0;
-        int sum = 0;
-        scanf("%d", &A);
+    scanf("%d", &A);
+    for (int i = 0; i < A; ++i) {
+        int number = 0;
 
-        if (A == -1)break;
+        scanf("%d", &number);
 
-        for (int i = 1; i < A; ++i) {
-            if (A % i == 0) {
-                arr[count] = i;
-                count++;
-                sum += i;
-            }
+        if (number == 1) continue;
+
+        int prime = 0;
+        for (int j = 1; j <= number; ++j) {
+            if (number % j == 0) prime++;
         }
-
-        if (sum == A) {
-            printf("%d =", A);
-            for (int i = 0; i < count; ++i) {
-                if (i == count - 1) {
-                    printf(" %d", arr[i]);
-                } else {
-                    printf(" %d +", arr[i]);
-                }
-
-            }
-            printf("\n");
-        } else {
-            printf("%d is NOT perfect.\n", A);
-        }
-
+        if (prime == 2) count++;
     }
+    printf("%d", count);
 
     return 0;
 }
