98aaa048aa1934eb1fb30eae70019eb57cf3a66b
mungdori <mungdori999@gmail.com>
2025-11-20 16:36:39 +0900

    14215

--- main.c
+++ main.c
@@ -2,30 +2,25 @@
 
 int main(void) {
 
-    while (1) {
-        int a, b, c = 0;
-        scanf("%d %d %d", &a, &b, &c);
+    int height[3] = {0,};
+    scanf("%d %d %d", &height[0], &height[1], &height[2]);
 
-        if (a == 0) break;
+    int sum = height[0] + height[1] + height[2];
+    int max = 0;
+    int num = 0;
 
-        int sum = a + b + c;
-        int max = 0;
+    for (int i = 0; i < 3; ++i) {
+        if (height[i] >= max){
+            max = height[i];
+            num = i;}
 
-        if (a >= b) {
-            if (a >= c) max = a;
-            else max = c;
-        } else {
-            if (b >= c) max = b;
-            else max = c;
-        }
+    }
 
-        if ((sum - max) > max) {
-            if (a == b && b == c && a == c) printf("Equilateral\n");
-            else if(a == b || b == c || a == c) printf("Isosceles\n");
-            else printf("Scalene\n");
-        } else {
-            printf("Invalid\n");
-        }
+    if ((sum - max) <= max) {
+        height[num] = (sum - max - 1);
+        printf("%d", height[0] + height[1] + height[2]);
+    } else {
+        printf("%d", sum);
     }
     return 0;
 }
