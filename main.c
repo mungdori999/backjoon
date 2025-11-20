54018a72e960587ca28b020226173612019d2ce4
mungdori <mungdori999@gmail.com>
2025-11-20 15:00:12 +0900

    15894

--- main.c
+++ main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int x[1001] = {0,};
-    int y[1001] = {0,};
+    long n;
+    scanf("%ld",&n);
 
-    for (int i = 0; i < 3; ++i) {
-        int coorX, coorY;
-        scanf("%d %d", &coorX, &coorY);
-        x[coorX]++;
-        y[coorY]++;
-    }
-    for (int i = 1; i <= 1000; ++i) {
-        if (x[i] == 1) {
-            printf("%d ", i);
-        }
-    }
-    for (int i = 1; i <= 1000; ++i) {
-        if (y[i] == 1) {
-            printf("%d", i);
-        }
-    }
-
+    printf("%ld",n*4);
     return 0;
 }
