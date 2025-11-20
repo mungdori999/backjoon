ff665e85cb63a84a5b4395e30b24def0f67671f2
mungdori <mungdori999@gmail.com>
2025-11-20 18:04:16 +0900

    24313

--- main.c
+++ main.c
@@ -2,10 +2,16 @@
 
 int main(void) {
 
-    long n;
-    scanf("%ld", &n);
+    int a1, a0, c, n0;
+    float n = 0;
 
-    printf("%ld\n", ((n - 2) * (n - 1) * (n)) / 6);
-    printf("3");
+    scanf("%d %d\n%d\n%d", &a1, &a0, &c, &n0);
+    int tmp = a1 - c;
+    if (tmp >= 0) n = (float) a0 / (float) tmp;
+    else n = (float) a0 / (float) (-tmp);
+
+    if (c >= a1)printf("%d", n <= (float) n0 ? 1 : 0);
+    else printf("0");
+
     return 0;
 }
