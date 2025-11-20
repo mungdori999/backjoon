63a0929d722990a9f9c9fffdc2ed6b1cad668538
mungdori <mungdori999@gmail.com>
2025-11-20 16:48:40 +0900

    24262

--- main.c
+++ main.c
@@ -2,25 +2,10 @@
 
 int main(void) {
 
-    int height[3] = {0,};
-    scanf("%d %d %d", &height[0], &height[1], &height[2]);
+    int n;
+    scanf("%d", &n);
 
-    int sum = height[0] + height[1] + height[2];
-    int max = 0;
-    int num = 0;
-
-    for (int i = 0; i < 3; ++i) {
-        if (height[i] >= max){
-            max = height[i];
-            num = i;}
-
-    }
-
-    if ((sum - max) <= max) {
-        height[num] = (sum - max - 1);
-        printf("%d", height[0] + height[1] + height[2]);
-    } else {
-        printf("%d", sum);
-    }
+    printf("1\n");
+    printf("0");
     return 0;
 }
