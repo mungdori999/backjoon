46bf19ee7c65a5ccd5b966db5dd589fad9ac8546
mungdori <mungdori999@gmail.com>
2025-11-20 15:35:48 +0900

    9063

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-    long n;
-    scanf("%ld",&n);
+    int n;
+    scanf("%d", &n);
+    int maxX = -10000;
+    int minX = 10000;
 
-    printf("%ld",n*4);
+    int maxY = -10000;
+    int minY = 10000;
+
+    for (int i = 0; i < n; ++i) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        maxX = maxX <= x ? x : maxX;
+        maxY = maxY <= y ? y : maxY;
+
+        minX = minX >= x ? x : minX;
+        minY = minY >= y ? y : minY;
+    }
+
+    printf("%d", (maxX - minX) * (maxY - minY));
     return 0;
 }
