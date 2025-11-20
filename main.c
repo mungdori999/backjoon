a27254382f882efb9fe486e7cd49ec09495075b7
mungdori <mungdori999@gmail.com>
2025-11-20 14:06:05 +0900

    11653

--- main.c
+++ main.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-    int A, B;
-    int prime[10000] = {0,};
-    int count = 0;
-    int sum = 0;
-    scanf("%d %d", &A, &B);
+    int N;
 
-    for (int i = A; i <= B; ++i) {
-        int j;
-        for (j = 2; j < i; ++j) {
-            if (i % j == 0) break;
+    scanf("%d", &N);
+
+    while (N > 1) {
+
+        int num = 2;
+        while (1) {
+            if (N % num == 0) {
+                N /= num;
+                printf("%d\n", num);
+                break;
+            }
+            num++;
         }
 
-        if (j == i) {
-            prime[count] = i;
-            count++;
-            sum += i;
-        }
     }
-
-    if (prime[0] == 0) printf("-1");
-    else {
-        printf("%d\n%d", sum, prime[0]);
-    }
-
     return 0;
 }
