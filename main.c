ae0f786aaa04f04708e0c92b4fc8a36673065f8d
mungdori <mungdori999@gmail.com>
2025-11-24 13:41:43 +0900

    2798

--- main.c
+++ main.c
@@ -2,16 +2,30 @@
 
 int main(void) {
 
-    int a1, a0, c, n0;
-    float n = 0;
+    int n, m;
+    int cards[101];
 
-    scanf("%d %d\n%d\n%d", &a1, &a0, &c, &n0);
-    int tmp = a1 - c;
-    if (tmp >= 0) n = (float) a0 / (float) tmp;
-    else n = (float) a0 / (float) (-tmp);
+    scanf("%d %d", &n, &m);
 
-    if (c >= a1)printf("%d", n <= (float) n0 ? 1 : 0);
-    else printf("0");
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &cards[i]);
+    }
+
+    int sum = 0;
+    int diff = 100000;
+
+    for (int i = 0; i < n - 2; ++i) {
+        for (int j = i + 1; j < n - 1; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                sum = cards[i] + cards[j] + cards[k];
+                if ((m - sum) >= 0 && diff >= (m - sum)) {
+                    diff = (m - sum);
+                }
+            }
+        }
+    }
+    printf("%d", (m - diff));
+
 
     return 0;
 }
