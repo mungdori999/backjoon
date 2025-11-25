fc612a3f5655fe9422243f9891caed34c2a171d6
mungdori <mungdori999@gmail.com>
2025-11-25 18:41:42 +0900

    2839

--- main.c
+++ main.c
@@ -1,36 +1,80 @@
 #include <stdio.h>
 
+int fiveFirst(int N);
+
+int threeFirst(int N);
+
 int main(void) {
 
     int N;
     scanf("%d", &N);
 
-    int count = 0;
-    int number = 1;
 
-    while (1) {
+    int i = fiveFirst(N);
+    int j = threeFirst(N);
 
-        int temp = number;
-        int checkTemp = 0;
-        while (temp != 0) {
-            if (temp % 10 == 6) checkTemp++;
-            else checkTemp = 0;
-
-            if (checkTemp == 3) {
-                count++;
-                break;
-            }
-            temp /= 10;
+    if (i == -1) {
+        if (j == -1) printf("-1");
+        else printf("%d", j);
+    } else {
+        if (j == -1) printf("%d", i);
+        else {
+            printf("%d", i >= j ? j : i);
         }
-
-
-        if (count == N) {
-            printf("%d", number);
-            break;
-        }
-        number++;
     }
 
-
     return 0;
+}
+
+int fiveFirst(int N) {
+
+    int min = 10000;
+    int i = 0;
+
+    while ((N - (5 * i)) >= 0) {
+        int five, three;
+        int temp = N;
+        five = i;
+        temp = temp - (5 * i);
+
+        three = temp / 3;
+
+        temp %= 3;
+        if (temp == 0) {
+            i++;
+            min = min >= five + three ? five + three : min;
+        } else {
+            i++;
+        }
+    }
+    if (min == 10000) return -1;
+    else return min;
+}
+
+
+int threeFirst(int N) {
+
+    int min = 10000;
+    int i = 0;
+
+    while ((N - (3 * i)) >= 0) {
+        int five, three;
+        int temp = N;
+
+        three = i;
+        temp = temp - (3 * i);
+
+        five = temp / 5;
+
+        temp %= 5;
+        if (temp == 0) {
+            i++;
+            min = min >= five + three ? five + three : min;
+        } else {
+            i++;
+
+        }
+    }
+    if (min == 10000) return -1;
+    else return min;
 }
