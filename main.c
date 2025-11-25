199a3d5a81b54f79ae81d9ab89e6065a47464855
mungdori <mungdori999@gmail.com>
2025-11-25 17:50:05 +0900

    1436

--- main.c
+++ main.c
@@ -1,61 +1,36 @@
 #include <stdio.h>
 
-int checkChess(const char board[51][51], int i, int j, char start);
-
 int main(void) {
 
-    int N, M;
-    char board[51][51];
-
-    scanf("%d %d", &N, &M);
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            scanf(" %c", &board[i][j]);
-        }
-    }
-
-    int min = 64;
-
-    for (int i = 0; i <= N - 8; ++i) {
-        for (int j = 0; j <= M - 8; ++j) {
-
-            int a = checkChess(board, i, j, 'W');
-            int b = checkChess(board, i, j, 'B');
-            int count = (a < b ? a : b);
-
-            if (min >= count) min = count;
-
-        }
-    }
-
-    printf("%d", min);
-    return 0;
-}
-
-int checkChess(const char board[51][51], int i, int j, char start) {
+    int N;
+    scanf("%d", &N);
 
     int count = 0;
-    char chess[8][8];
+    int number = 1;
 
-    chess[0][0] = start;
+    while (1) {
 
-    for (int k = 0; k < 8; ++k) {
-        for (int l = 0; l < 8; ++l) {
-            if (k == 0 && l == 0) {
-                chess[k][l] == board[i + k][j + l] ? count : count++;
-                continue;
+        int temp = number;
+        int checkTemp = 0;
+        while (temp != 0) {
+            if (temp % 10 == 6) checkTemp++;
+            else checkTemp = 0;
+
+            if (checkTemp == 3) {
+                count++;
+                break;
             }
-
-            if (l == 0) {
-                chess[k][l] = chess[k - 1][l] == 'B' ? 'W' : 'B';
-                chess[k][l] == board[i + k][j + l] ? count : count++;
-
-            } else {
-                chess[k][l] = chess[k][l - 1] == 'B' ? 'W' : 'B';
-                chess[k][l] == board[i + k][j + l] ? count : count++;
-            }
+            temp /= 10;
         }
+
+
+        if (count == N) {
+            printf("%d", number);
+            break;
+        }
+        number++;
     }
-    return count;
+
+
+    return 0;
 }
