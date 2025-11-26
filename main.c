3dd91622552a2c808b44f4feeb872b2ce26b97ea
mungdori <mungdori999@gmail.com>
2025-11-26 16:31:22 +0900

    2750

--- main.c
+++ main.c
@@ -1,80 +1,27 @@
 #include <stdio.h>
 
-int fiveFirst(int N);
-
-int threeFirst(int N);
 
 int main(void) {
 
     int N;
+    int arr[1001] = {0,};
     scanf("%d", &N);
-
-
-    int i = fiveFirst(N);
-    int j = threeFirst(N);
-
-    if (i == -1) {
-        if (j == -1) printf("-1");
-        else printf("%d", j);
-    } else {
-        if (j == -1) printf("%d", i);
-        else {
-            printf("%d", i >= j ? j : i);
-        }
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &arr[i]);
     }
 
+    for (int i = 0; i < N - 1; ++i) {
+
+        for (int j = 0; j < N - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        printf("%d\n",arr[i]);
+    }
     return 0;
-}
-
-int fiveFirst(int N) {
-
-    int min = 10000;
-    int i = 0;
-
-    while ((N - (5 * i)) >= 0) {
-        int five, three;
-        int temp = N;
-        five = i;
-        temp = temp - (5 * i);
-
-        three = temp / 3;
-
-        temp %= 3;
-        if (temp == 0) {
-            i++;
-            min = min >= five + three ? five + three : min;
-        } else {
-            i++;
-        }
-    }
-    if (min == 10000) return -1;
-    else return min;
-}
-
-
-int threeFirst(int N) {
-
-    int min = 10000;
-    int i = 0;
-
-    while ((N - (3 * i)) >= 0) {
-        int five, three;
-        int temp = N;
-
-        three = i;
-        temp = temp - (3 * i);
-
-        five = temp / 5;
-
-        temp %= 5;
-        if (temp == 0) {
-            i++;
-            min = min >= five + three ? five + three : min;
-        } else {
-            i++;
-
-        }
-    }
-    if (min == 10000) return -1;
-    else return min;
 }
