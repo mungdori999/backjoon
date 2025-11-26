1028296c451bea994b4108262f766067d300e336
mungdori <mungdori999@gmail.com>
2025-11-26 17:26:57 +0900

    2751

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main(void) {
 
-    int N, k;
-    int arr[1001];
+    int N;
+    int arr[2000002];
+    memset(arr, 0, sizeof(arr));
 
-    scanf("%d %d", &N, &k);
+    scanf("%d", &N);
 
     for (int i = 0; i < N; ++i) {
-        scanf("%d", &arr[i]);
+        int temp;
+        scanf("%d", &temp);
+        arr[temp + 1000000] = 1;
+
     }
 
-    for (int i = 0; i < N - 1; ++i) {
-        for (int j = 0; j < N - i - 1; ++j) {
-            if (arr[j] < arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+    for (int i = 0; i < 2000002; ++i) {
+        if (arr[i]) printf("%d\n", i-1000000);
     }
 
-    printf("%d", arr[k-1]);
 
     return 0;
 }
