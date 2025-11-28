3400c394a6aaeca5d2ba068162c9492e403a13f5
mungdori <mungdori999@gmail.com>
2025-11-28 13:56:24 +0900

    10989

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-#include <string.h>
 
+int arr[10001];  // 전역 -> 자동 초기화
 
 int main(void) {
-
     int N;
-    int arr[2000002];
-    memset(arr, 0, sizeof(arr));
 
     scanf("%d", &N);
 
     for (int i = 0; i < N; ++i) {
-        int temp;
-        scanf("%d", &temp);
-        arr[temp + 1000000] = 1;
-
+        int tmp;
+        scanf("%d", &tmp);
+        arr[tmp]++;
     }
 
-    for (int i = 0; i < 2000002; ++i) {
-        if (arr[i]) printf("%d\n", i-1000000);
+    for (int i = 1; i <= 10000; ++i) {
+        while (arr[i]--) printf("%d\n", i);
     }
-
-
     return 0;
 }
